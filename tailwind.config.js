1089bcaa30ccfe2844bca0ec1123411894ea4955
Ritom Sen <ritomsen@Tem-Bot-2.local>
2024-02-28 20:59:49 -0500

    changes for smaller size screens

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./api/**/*.{html,js}"],
+  theme: {
+    extend: {
+      divideColor: ['group-hover'],
+    }
+  },
+  plugins: [],
+}
+
